02f053290f02040554b0a9233e4fccd4dc511f84
doctorbingchilling <118613097+doctorbingchilling@users.noreply.github.com>
2023-09-26 15:35:33 +0700

    Added a Cartesian Tree (Treap)

---
+++ Data-Structures/NOTEBOOK.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+struct item{
+    int key, prior;
+    item *l, *r;
+    item () { }
+    item (int key) : key(key), prior(rand()), l(NULL), r(NULL) { }
+    item (int key, int prior) : key(key), prior(prior), l(NULL), r(NULL) { }
+};
+typedef item* pitem;
+void split (pitem t, int key, pitem & l, pitem & r) {
+    if (!t)
+        l = r = NULL;
+    else if (t->key <= key)
+        split (t->r, key, t->r, r),  l = t;
+    else
+        split (t->l, key, l, t->l),  r = t;
+}
+void insert (pitem & t, pitem it) {
+    if (!t)
+        t = it;
+    else if (it->prior > t->prior)
+        split (t, it->key, it->l, it->r),  t = it;
+    else
+        insert (t->key <= it->key ? t->r : t->l, it);
+}
+void merge (pitem & t, pitem l, pitem r) {
+    if (!l || !r)
+        t = l ? l : r;
+    else if (l->prior > r->prior)
+        merge (l->r, l->r, r),  t = l;
+    else
+        merge (r->l, l, r->l),  t = r;
+}
+void erase (pitem & t, int key) {
+    if (t->key == key) {
+        pitem th = t;
+        merge (t, t->l, t->r);
+        delete th;
+    }
+    else
+        erase (key < t->key ? t->l : t->r, key);
+}
+pitem unite (pitem l, pitem r) {
+    if (!l || !r)  return l ? l : r;
+    if (l->prior < r->prior)  swap (l, r);
+    pitem lt, rt;
+    split (r, l->key, lt, rt);
+    l->l = unite (l->l, lt);
+    l->r = unite (l->r, rt);
+    return l;
+}
+pitem search(pitem t, int key){
+    if(t == NULL || t->key == key){
+        return t;
+    }
+    if(t->key < key){
+        return search(t->l,key);
+    }
+    return search(t->r,key);
+}
+pitem newItem(int key){
+    pitem temp = new item(key);
+    temp->l = NULL, temp->r = NULL;
+    return temp;
+}
+int main(){
+    pitem t = NULL;
+    cout << (!t) << '\n';
+}
